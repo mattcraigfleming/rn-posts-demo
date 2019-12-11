78a09a0b111d177a693fb2eab2659e825a31abb1
mattcraigfleming <mattcfleming@outlook.com>
2019-12-11 18:05:35 +0000

    Jest mocks added for async storage and gesture handler

--- src/screens/PostDetails.spec.js
+++ src/screens/PostDetails.spec.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import configureStore from 'redux-mock-store';
+import App from '../App';
+import Posts from './Posts';
+import PostDetails from './PostDetails';
 
 jest.mock('react-native/Libraries/Lists/FlatList', () => 'FlatList');
 
-// imported as a connected component!
-import App from '../App';
-
-const middlewares = []; // you can mock any middlewares here if necessary
+const middlewares = [];
 const mockStore = configureStore(middlewares);
 
 const initialState = {
@@ -16,13 +16,16 @@ const initialState = {
 
 describe('Testing Authors', () => {
   it('renders as expected', () => {
-    const wrapper = shallow(<App />, {
-      context: {store: mockStore(initialState)},
-    }).dive();
+    const wrapper = shallow(
+      <App>
+        <Posts>
+          <PostDetails />
+        </Posts>
+      </App>,
+      {
+        context: {store: mockStore(initialState)},
+      },
+    ).dive();
     expect(wrapper.dive()).toMatchSnapshot();
   });
 });
-
-//   const leanne = {id: 1, name: 'Leanne Graham'};
-//   const ervin = {id: 2, name: 'Ervin Howell'};
-//   const authors = [leanne, ervin];
--- src/screens/Posts.spec.js
+++ src/screens/Posts.spec.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import configureStore from 'redux-mock-store';
+import App from '../App';
+import Posts from './Posts';
 
 jest.mock('react-native/Libraries/Lists/FlatList', () => 'FlatList');
 
-// imported as a connected component!
-import App from '../App';
-
-const middlewares = []; // you can mock any middlewares here if necessary
+const middlewares = [];
 const mockStore = configureStore(middlewares);
 
 const initialState = {
@@ -16,34 +15,15 @@ const initialState = {
 
 describe('Testing Posts', () => {
   it('renders as expected', () => {
-    const wrapper = shallow(<App />, {
-      context: {store: mockStore(initialState)},
-    }).dive();
+    const wrapper = shallow(
+      <App>
+        <Posts />
+      </App>,
+      {
+        context: {store: mockStore(initialState)},
+      },
+    ).dive();
+    console.log(wrapper.debug());
     expect(wrapper.dive()).toMatchSnapshot();
   });
 });
-
-//   const posts = [
-//     {
-//       userId: 1,
-//       id: 1,
-//       title:
-//         'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
-//       body:
-//         'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
-//     },
-//     {
-//       userId: 1,
-//       id: 2,
-//       title: 'qui est esse',
-//       body:
-//         'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla',
-//     },
-//     {
-//       userId: 1,
-//       id: 3,
-//       title: 'ea molestias quasi exercitationem repellat qui ipsa sit aut',
-//       body:
-//         'et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut',
-//     },
-//   ];
